205e7265d081a294c6dc00e42c08dee4de1a37b1
Himanshu Singh <himanshusingh1k@gmail.com>
2025-12-23 11:42:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
+Object.prototype.getKeys = function () {
+  return Object.keys(this);
+};
+
 const student = {
   name: "John",
   age: 23,
   city: "Manhattan",
-  getKeys() {
-    return Object.keys(this);
-  },
 };
 
-console.log(student.getKeys()); // ["name", "age", "city"]
+console.log(student.getKeys());
