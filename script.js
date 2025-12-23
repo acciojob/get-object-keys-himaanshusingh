3f1b0149c4f731a60b27194c96c5c054cecb7e5d
Himanshu Singh <himanshusingh1k@gmail.com>
2025-12-23 11:28:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-//your JS code here. If required.
-// Create the student object
+// your JS code here. If required.
 const student = {
   name: "John",
   age: 23,
   city: "Manhattan",
+  getKeys: function (obj) {
+    return Object.keys(obj);
+  },
 };
-
-// Function to get all keys from the object
-function getKeys(obj) {
-  return Object.keys(obj);
-}
